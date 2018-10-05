a402e8a40e7e24016b1039fdc5c57ef0f1a158c4
rocking <rocking@bitmain.com>
2018-10-05 11:25:07 +0800

    1. Fix some freeze bug 2.  Remove num_output in param

--- util/keras_to_tensorflow.py
+++ util/keras_to_tensorflow.py
@@ -7,8 +7,7 @@ from tensorflow.python.framework import graph_io
 from tensorflow.tools.graph_transforms import TransformGraph
 
 
-def save_frozen_tf_model(keras_model, tf_model_name, dst_dir='.',
-                         num_output=1, graph_def=False,
+def save_frozen_tf_model(keras_model, tf_model_name, dst_dir='.', graph_def=False,
                          output_graphdef_name='model.ascii', quantize=False):
     if dst_dir != '.':
         Path(dst_dir).mkdir(parents=True, exist_ok=True)
@@ -18,16 +17,25 @@ def save_frozen_tf_model(keras_model, tf_model_name, dst_dir='.',
         os.remove(tf_model_full_path)
 
     keras_pred_node_names = [x.op.name for x in keras_model.outputs]
-    pred = [None] * num_output
-    pred_node_names = [None] * num_output
-
-    for i in range(num_output):
-        pred_node_names[i] = keras_pred_node_names[i]
-        pred[i] = tf.identity(keras_model.outputs[i], name=pred_node_names[i])
-
-    print('output nodes names are: ', pred_node_names)
+    print('output nodes names are: ', keras_pred_node_names)
 
     sess = K.get_session()
+    # get graph definition
+    gd = sess.graph.as_graph_def()
+
+    # fix bug. Ref: https://github.com/tensorflow/tensorflow/issues/3628
+    for node in gd.node:
+        if node.op == 'RefSwitch':
+            node.op = 'Switch'
+            for index in range(len(node.input)):
+                if 'moving_' in node.input[index]:
+                    node.input[index] = node.input[index] + '/read'
+        elif node.op == 'AssignSub':
+            node.op = 'Sub'
+            if 'use_locking' in node.attr: del node.attr['use_locking']
+        elif node.op == 'AssignAdd':
+            node.op = 'Add'
+            if 'use_locking' in node.attr: del node.attr['use_locking']
 
     # write graph definition in ascii
     if graph_def:
@@ -35,16 +43,11 @@ def save_frozen_tf_model(keras_model, tf_model_name, dst_dir='.',
         if os.path.exists(graph_def_full_path):
             os.remove(graph_def_full_path)
 
-        tf.train.write_graph(sess.graph.as_graph_def(), dst_dir, output_graphdef_name, as_text=True)
+        tf.train.write_graph(gd, dst_dir, output_graphdef_name, as_text=True)
         print('saved the graph definition in ascii format at: ', graph_def_full_path)
 
     # convert variables to constants and save
-    if quantize:
-        transforms = ["quantize_weights", "quantize_nodes"]
-        transformed_graph_def = TransformGraph(sess.graph.as_graph_def(), [], pred_node_names, transforms)
-        constant_graph = graph_util.convert_variables_to_constants(sess, transformed_graph_def, pred_node_names)
-    else:
-        constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), pred_node_names)
+    constant_graph = graph_util.convert_variables_to_constants(sess, gd, keras_pred_node_names)
 
     graph_io.write_graph(constant_graph, dst_dir, tf_model_name, as_text=False)
     print('saved the freezed graph (ready for inference) at: ', tf_model_full_path)
